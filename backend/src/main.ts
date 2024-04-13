e17977340f58c0c38ab08c1bf10c60b2faa18f5f
Xcyth <ritam.choudhuri@gmail.com>
2024-04-13 06:30:35 +0530

    1 little meaningless change

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,6 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { Logger } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -17,6 +18,7 @@ async function bootstrap() {
   SwaggerModule.setup('api', app, document);
 
   await app.listen(port);
-  console.log(`Application is running on: http://localhost:${port}`);
+  const url = await app.getUrl();
+  Logger.log(`Application is running on: ${url}`, 'NestApplication');
 }
 bootstrap();
